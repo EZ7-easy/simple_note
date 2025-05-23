4a3b90f69cb7823f9545c5d35245065f2a8a0605
EZ7-easy <b.sh.tutor@gmail.com>
2025-05-23 22:27:33 +0500

    3

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,11 +1,18 @@
-import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+type Params = {
+  params: {
+    id: string;
+  };
+};3
 
 export async function DELETE(
-  _req: Request,
-  context: { params: { id: string } }
+  _req: NextRequest,
+  { params }: Params
 ) {
-  const id = parseInt(context.params.id);
+  const id = parseInt(params.id);
 
   if (isNaN(id)) {
     return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
